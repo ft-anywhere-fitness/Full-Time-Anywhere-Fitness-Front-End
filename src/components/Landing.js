bd3c6e371a69407a7b1d1c0e6fbad57f856e7f82
Ade <shennaike2386@hotmail.com>
2021-11-15 16:04:42 -0500

    finished the frame work of the component, will add the main image section to it for the next commit

---
+++ src/components/Landing.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Landing() {
+    return(
+        <div className = "quickLooks">
+            <div className = "first section">
+                <div className = "img">
+                    <img src = "" alt = ""/>
+                </div>
+                <div className = "paragraph">
+                    <h2></h2>
+                    <p> </p>
+                </div>
+            </div>
+
+            <div className = "second section">
+                <div className = "paragraph">
+                    <p> </p>
+                    <h2></h2>
+                </div>
+                <div className = "img">
+                    <img src = "" alt = ""/>
+                </div>
+            </div>
+
+            <div className = "third section">
+                <div className = "img">
+                    <img src = "" alt = ""/>
+                </div>
+                <div className = "paragraph">
+                    <h2></h2>
+                    <p> </p>
+                </div>
+            </div>
+        </div>
+    )
+}

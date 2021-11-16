69a8e713377a36d674649773c1828afd901de3ca
Adam Hinton <adam.d.hinton@gmail.com>
2021-11-15 20:37:57 -0500

    put footer together, they use the same stylesheet. On small screens the header and footer get jumbled together, not sure how to fix that yet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom';
+import Header from './Header';
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm'
+
+// NOTES:
+// -This is mostly designed the same as the Header, and **uses the same styles as the header**.
+// -Each item in the footer has the class name 'nav-item'
 
 export default function Footer(){
     return(
         <footer>
-               <nav>
-                {/* <h1 className='nav-item'>Anywhere Fitness</h1> */}
+            {/* all the items in the footer will have the classname 'nav-item' */}
+               <nav className='footernav'>
                 <NavLink  className='nav-item underline'  to='/login'>
                     Login
                 </NavLink>
@@ -15,7 +20,9 @@ export default function Footer(){
                    Sign Up
                 </NavLink>
                 <a className='nav-item' href= 'https://github.com'>Github</a>
-                <h6 className='nav-item copyright'>&copy;Lorem Ipsum Industries</h6>
+                <a className = 'nav-item' href=''>Supplements</a>
+                <h6 className='nav-item copyright'>&copy;Lorem Ipsum Industries 2021</h6>
+
 
             </nav>
        

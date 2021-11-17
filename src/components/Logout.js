6375da59cf56bd48969bb565c65310132ed2fe0f
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-11-17 12:53:55 -0600

    Finished routing, beginnig to work on EditClass and Logout, I'll stay on this branch for those

---
+++ src/components/Logout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useNavigate } from "react-router";
+import AxiosWithAuth from "../utils/AxiosWithAuth";
+
+const Logout = () => {
+    return (
+        <div>asdf</div>
+    )
+}
+
+export default Logout;

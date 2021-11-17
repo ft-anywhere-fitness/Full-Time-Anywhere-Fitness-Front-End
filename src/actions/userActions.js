2b7f3df9fae40e00e084e7a3e2899184ce741a9c
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-17 08:03:47 -0800

    Created new postLogin action for logging in

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,3 +1,29 @@
+// Libraries
+import axios from "axios";
+
+// Axios Call to Login
+export const postLogin = (credentials) => {
+  return (dispatch) => {
+    axios
+      .post("https://anywherefitnesslambda.herokuapp.com/api/auth/login", credentials)
+      .then((response) => {
+        localStorage.setItem("token", response.data.token);
+        axios
+          .get("https://anywherefitnesslambda.herokuapp.com/api/users")
+          .then((response) => {
+            const userData = response.data.find((user) => user.username === credentials.username);
+            dispatch(login(userData));
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      })
+      .then((error) => {
+        console.error(error);
+      });
+  };
+};
+
 // Constants
 export const LOGIN = "LOGIN";
 export const NEW_USER = "ADD_USER";

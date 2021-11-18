99adbff738864e1c5eb4d7a289496631b36f9651
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-11-18 17:36:52 -0600

    add

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -4,10 +4,14 @@ import { useNavigate } from "react-router-dom";
 const Logout = () => {
     const navigate = useNavigate();
 
+    const bye = () => {
+        navigate('/');
+    }
+
     useEffect(() => {
         localStorage.removeItem('token');
-        navigate("/");
-    }, []);
+        bye();
+    }, );
     return (
         <h1> Logging Out... </h1>
     )

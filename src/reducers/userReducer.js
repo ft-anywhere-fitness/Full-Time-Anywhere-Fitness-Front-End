49d7ab8aa8d60c0af066aebffbc5b94c579f3e04
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-18 18:11:21 -0800

    clearing reducers and actions

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,47 +1,14 @@
-// Constants
-import {
-  REGISTERING,
-  SUCCESSFUL_LOGIN,
-  SUCCESSFUL_REGISTER,
-  FAILED_REGISTER
-} from "../actions/userActions";
-
 // Initial State
 const initialState = {
   "username": "",
   "email": "",
   "role_id": null,
-  "auth": "",
-  "isRegistering": false,
-  "registrationError": "",
-  "isLoggingIn": false
+  "auth": ""
 };
 
 // User Reducer
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case REGISTERING:
-      return {
-        ...state,
-        "isRegistering": true,
-        "registrationError": ""
-      };
-    case SUCCESSFUL_REGISTER:
-      return {
-        ...state,
-        "isRegistering": false
-      };
-    case FAILED_REGISTER:
-      return {
-        ...state,
-        "isRegistering": false,
-        "registrationError": "Failed to register &#9785;"
-      };
-    case SUCCESSFUL_LOGIN:
-      return {
-        ...state,
-        ...action.payload
-      };
     default:
       return state;
   }
